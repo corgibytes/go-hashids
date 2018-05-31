51ce8fbefc7e092f4a22d0a1f880d0cd4318da1c
M. Scott Ford <scott@corgibytes.com>
2018-05-31 15:03:34 -0400

    Adds tests for ensuring default alphabet is set correctly

--- hashids_test.go
+++ hashids_test.go
@@ -1,8 +1,50 @@
 package hashids
 
 import (
+	"reflect"
 	"testing"
 )
+
+func assertEqual(t *testing.T, a interface{}, b interface{}) {
+	result := false
+
+	if reflect.TypeOf(a).Kind() == reflect.Slice {
+		result = reflect.DeepEqual(a, b)
+	} else {
+		result = (a == b)
+	}
+
+	if !(result) {
+		t.Fatalf("%s != %s", a, b)
+	}
+}
+
+func assertError(t *testing.T, error interface{}) {
+	if error != nil {
+		t.Fatal(error)
+	}
+}
+
+func TestSalt(t *testing.T) {
+	hid := New()
+	hid.Salt = "Arbitrary string"
+
+	hash, err := hid.Encrypt([]int{683, 94108, 123, 5})
+	assertError(t, err)
+	assertEqual(t, hash, "q9khp7X9u6BuE")
+
+	hash, err = hid.Encrypt([]int{1, 2, 3})
+	assertError(t, err)
+	assertEqual(t, hash, "a7tLSG")
+
+	hash, err = hid.Encrypt([]int{2, 4, 6})
+	assertError(t, err)
+	assertEqual(t, hash, "Xbh4fp")
+
+	hash, err = hid.Encrypt([]int{99, 25})
+	assertError(t, err)
+	assertEqual(t, hash, "K6nCz")
+}
 
 func TestEncryptDecrypt(t *testing.T) {
 	hid := New()
